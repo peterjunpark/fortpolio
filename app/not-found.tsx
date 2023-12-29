25b04a401cb13686d8ab135db5e1f58a51e89758
Peter Jun Park <park@hey.com>
2023-12-29 01:52:39 -0800

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import { PiSmileyNervous as OhShitIcon } from "react-icons/pi";
+
 export default function NotFound() {
-  return <div>404: Page not found</div>;
+  return (
+    <div className="flex flex-col items-center justify-center py-6 font-semibold">
+      <p>404: Page not found</p>
+      <Link href="/" className="p-6 text-6xl">
+        <OhShitIcon />
+      </Link>
+    </div>
+  );
 }
