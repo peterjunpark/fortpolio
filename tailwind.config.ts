63a187a1f8019e64380ca39264eee900b4955c79
Peter Jun Park <park@hey.com>
2023-12-28 16:53:13 -0800

    add fonts and colours to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
+    colors: {
+      base: "hsl(var(--base))",
+      surface: "hsl(var(--surface))",
+      overlay: "hsl(var(--overlay))",
+      muted: "hsl(var(--muted))",
+      subtle: "hsl(var(--subtle))",
+      text: "hsl(var(--text))",
+      love: "hsl(var(--love))",
+      gold: "hsl(var(--gold))",
+      rose: "hsl(var(--rose))",
+      pine: "hsl(var(--pine))",
+      foam: "hsl(var(--foam))",
+      iris: "hsl(var(--iris))",
+      highlight: {
+        DEFAULT: "hsl(var(--highlight-md))",
+        lo: "hsl(var(--highlight-lo))",
+        hi: "hsl(var(--highlight-hi))",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-overpass)"],
+        mono: ["var(--font-overpass-mono)"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
