bd21eb27b6deb631dfdc59b340ef9928affbda34
Peter Jun Park <park@hey.com>
2023-12-30 21:11:47 -0800

    fix github stats images

--- components/github-stats-card.tsx
+++ components/github-stats-card.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
 import { siteInfo } from "@/site-config";
 
@@ -26,33 +27,38 @@ export function GithubStatsCard({ variant, repo }: GithubStatsCardProps) {
   let href = `https://github.com/${username}/`;
   let src = `?username=${username}&hide=stars&rank_icon=github&custom_title=${username}`;
   let alt = `GitHub stats for ${username}`;
-  let width = 420;
+  let widthClass = "w-full";
 
   switch (true) {
     case variant === "lang":
       src = `/top-langs?username=${username}&hide_progress=true&hide=c%2B%2B,cmake&langs_count=8`;
       alt = `GitHub language stats for ${username}`;
-      width = 340;
+      widthClass = "w-[21rem]";
       break;
     case variant === "repo":
       href += repo;
       src = `/pin?username=${username}&repo=${repo}`;
       alt = "GitHub stats for ${username}'s ${repo} repository";
-      width = 400;
-      break;
+      widthClass = "w-[26rem]";
   }
 
   return (
-    <Link href={href} className="my-6 flex justify-center">
-      <Image
-        loader={imageLoader}
-        src={src}
-        alt={alt}
-        width={width}
-        height={0}
-        placeholder="blur"
-        blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNMqQcAAU0A5aSPMbMAAAAASUVORK5CYII="
-      />
-    </Link>
+    <span className="flex w-full justify-center py-5">
+      <Link href={href}>
+        <Image
+          loader={imageLoader}
+          src={src}
+          alt={alt}
+          width={0}
+          height={0}
+          placeholder="blur"
+          blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNMqQcAAU0A5aSPMbMAAAAASUVORK5CYII="
+          className={cn(
+            "rounded-md outline-1 outline-rose hover:outline dark:outline-love",
+            widthClass,
+          )}
+        />
+      </Link>
+    </span>
   );
 }
